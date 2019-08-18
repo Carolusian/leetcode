130e9879d7498147d4770e00a0a57601acb89447
carolusian <pkufashuo400@gmail.com>
2019-08-18 12:31:50 +0800

    Add weekly-contest-150

---
+++ weekly-contest-150/find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,53 @@
+"""
+5048. Find Words That Can Be Formed by Characters
+
+You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once).
+
+Return the sum of lengths of all good strings in words.
+
+ 
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: 
+The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+Example 2:
+
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: 
+The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+
+Note:
+
+1 <= words.length <= 1000
+1 <= words[i].length, chars.length <= 100
+All strings contain lowercase English letters only.
+"""
+from typing import List
+
+
+def countCharacters(words: List[str], chars: str) -> int:
+    ret = []
+    for w in words:
+        l = 0
+        characters = chars
+        for c in w:
+            idx = characters.find(c)
+            if idx > -1:
+                l += 1
+                characters = characters[0: idx] + characters[idx + 1:]
+        if l == len(w):
+            ret.append(len(w))
+    return sum(ret)
+            
+
+print(countCharacters(["cat","bt","hat","tree"], "atach"))
+print(countCharacters(["dyiclysmffuhibgfvapygkorkqllqlvokojsagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"], "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp"))
+
+# Or, just count the frequency of each character in the words, and that of the chars 
