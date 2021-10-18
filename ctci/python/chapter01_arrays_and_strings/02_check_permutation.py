20a2800caa503bb7acf8286d278e787224374c6b
carolusian <pkufashuo400@gmail.com>
2021-10-19 01:35:46 +0800

    counter can be compared

--- ctci/python/chapter01_arrays_and_strings/02_check_permutation.py
+++ ctci/python/chapter01_arrays_and_strings/02_check_permutation.py
@@ -8,24 +8,24 @@ from itertools import permutations
 class Test(unittest.TestCase):
     def test_check_perm(self):
         s1, s2 = "abc", "cba"
-        self.assertTrue(check_perm(s1, s2))
+        self.assertTrue(check_permutation(s1, s2))
 
         s1, s2 = "abcdjfklsjfsdkl", "fkdsjfwdofjdskfdj"
-        self.assertFalse(check_perm(s1, s2))
+        self.assertFalse(check_permutation(s1, s2))
 
         s1, s2 = "abccd", "bccda"
-        self.assertTrue(check_perm(s1, s2))
+        self.assertTrue(check_permutation(s1, s2))
 
         s1 = "ewofdsggk"
         for p in permutations(s1):
             s2 = "".join(p)
-            self.assertTrue(check_perm(s1, s2))
+            self.assertTrue(check_permutation(s1, s2))
 
         s1, s2 = "hello", "ooleh"
-        self.assertFalse(check_perm(s1, s2))
+        self.assertFalse(check_permutation(s1, s2))
 
 
-def check_perm(s1: str, s2: str) -> bool:
+def check_permutation(s1: str, s2: str) -> bool:
     # a string is a permutation of itself
     if s1 == s2: 
         return True
@@ -33,17 +33,10 @@ def check_perm(s1: str, s2: str) -> bool:
     if len(s1) != len(s2): 
         return False
 
-    ctr1 = Counter(s1)
-    ctr2 = Counter(s2)
+    return Counter(s1) == Counter(s2)
 
-    all_chars = set(s1) | set(s2)
-    try:
-        for c in all_chars:
-            if ctr1[c] != ctr2[c]:
-                return False
-    except:
-        return False
-    return True
+# TODO: using list/array index to represent the character using ord
+
 
 if __name__ == "__main__":
     unittest.main()
