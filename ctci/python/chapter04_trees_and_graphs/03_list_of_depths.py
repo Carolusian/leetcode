02a6e78ca0d2b8d04489c0080553b57336b8df2e
carolusian <charliechen@lionfin.com>
2021-12-07 18:36:31 +0800

    add template for chapter04 03_list_of_depths

---
+++ ctci/python/chapter04_trees_and_graphs/03_list_of_depths.py
@@ -0,0 +1,13 @@
+# List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
+# at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+# Hints: #107, #123, #135
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    pass
